c7c8cb762b14581e4079edc9ff6ba57a51143725
meerkatstudio <patrick.lemmer@yahoo.com>
2022-06-05 21:56:25 +0200

    updated App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import './App.css';
-import Navbar from './components/Navbar';
-import Sidebar from './components/Sidebar';
+import Home from './pages';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
-      <Sidebar />
-      <Navbar />
+      <Home />
     </Router>
   );
 }
