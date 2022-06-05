19acdb5f25fdb27e129f21e69860a8adeadccb84
meerkatstudio <patrick.lemmer@yahoo.com>
2022-06-05 21:50:13 +0200

    integrated Sidebar into App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
+      <Sidebar />
       <Navbar />
     </Router>
   );
