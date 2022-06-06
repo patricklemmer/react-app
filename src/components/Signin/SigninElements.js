575929820c800fa2173b9cac9195700c5a82e7bd
meerkatstudio <patrick.lemmer@yahoo.com>
2022-06-06 16:57:53 +0200

    created and implemented SignIn element

---
+++ src/components/Signin/SigninElements.js
@@ -0,0 +1,108 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+export const Container = styled.div`
+  min-height: 692px;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  top: 0;
+  z-index: 0;
+  overflow: hidden;
+  background: linear-gradient(
+    180deg,
+    rgba(1, 147, 86, 1) 0%,
+    rgba(10, 201, 122, 1) 100%
+  );
+`;
+
+export const FormWrap = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  @media screen and (max-width: 400px) {
+    height: 80%;
+  }
+`;
+export const Icon = styled(Link)`
+  margin-left: 32px;
+  margin-top: 32px;
+  text-decoration: none;
+  color: #fff;
+  font-weight: 700;
+  font-size: 2rem;
+
+  @media screen and (max-width: 480px) {
+    margin-left: 1rem;
+    margin-top: 0.5rem;
+  }
+`;
+
+export const FormContent = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  @media screen and (max-width: 480px) {
+    padding: 10px;
+  }
+`;
+
+export const Form = styled.form`
+  background: #010101;
+  max-width: 400px;
+  height: auto;
+  width: 100%;
+  z-index: 1;
+  display: grid;
+  margin: 0 auto;
+  padding: 80px 32px;
+  border-radius: 4px;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.9);
+
+  @media screen and (max-width: 400px) {
+    padding: 2rem 2rem;
+  }
+`;
+
+export const FormH1 = styled.div`
+  margin-bottom: 40px;
+  color: #fff;
+  font-size: 20px;
+  font-weight: 400;
+  text-align: center;
+`;
+
+export const FormLabel = styled.label`
+  margin-bottom: 0.5rem;
+  font-size: 14px;
+  color: #fff;
+`;
+
+export const FormInput = styled.input`
+  padding: 1rem 1rem;
+  margin-bottom: 2rem;
+  border: none;
+  border-radius: 0.25rem;
+`;
+
+export const FormButton = styled.button`
+  background: #01bf71;
+  padding: 1rem 0;
+  border: none;
+  border-radius: 0.25rem;
+  color: #fff;
+  font-size: 20px;
+  cursor: pointer;
+`;
+
+export const Text = styled.span`
+  text-align: center;
+  margin-top: 24px;
+  color: #fff;
+  font-size: 14px;
+`;
