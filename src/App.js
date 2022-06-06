6f9f250d7545eec88b4655256e773b3c7debcea1
meerkatstudio <patrick.lemmer@yahoo.com>
2022-06-06 13:55:39 +0200

    created and implemented Signin Page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
 import Home from './pages';
-import { BrowserRouter as Router } from 'react-router-dom';
+import SigninPage from './pages/signin';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} exact />
+        <Route path="/signin" element={<SigninPage />} exact />
+      </Routes>
     </Router>
   );
 }
